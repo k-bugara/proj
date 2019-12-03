fb4677423681ff2248ea916b89381efb350cec86
k-bugara <kamil.bugara@gmail.com>
2019-12-03 12:03:38 +0100

    zmiana w pliku main

--- Documents/KB/git/main.cpp
+++ Documents/KB/git/main.cpp
@@ -11,16 +11,16 @@ return TString(c);
 
 int main ()
 {
-    TString s1;
-    TString s2("inicjalizacja slowem");
-   TString s3 = s2;
+    //TString s1;
+    TString s4("inicjalizacja slowem");
+//TString s3 = s2;
    // TString s3 (s2);
     // albo TString s3 {s2};
    // TString s3 = "alfa beta";
-     s3 = s2;
-    s3=std::move(s1);
-     TString s5=fun("konstruktor przenoszacy");
-     TString s6=s2;
-     s1 = "zebym nie byl pusty";
-     cout << "ta wartosc     "<< s1[3] << endl;
+   //  s3 = s2;
+    //s3=std::move(s1);
+     TString s5=fun("jakis napis");
+     TString s6=s4;
+    // s1 = "zebym nie byl pusty";
+    // cout << "ta wartosc     "<< s1[3] << endl;
 }
